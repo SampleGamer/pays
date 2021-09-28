7a5f54932f9175e194024c1a78bcb31aca66c2dc
Jeremy <jerpar2002@gmail.com>
2021-09-28 11:01:30 -0400

    Debut

---
+++ Les structures séquentielles et alternatives/Les structures séquentielles et alternatives/Les structures séquentielles et alternatives.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+namespace Les_structures_séquentielles_et_alternatives
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculerNote = new System.Windows.Forms.Button();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtMatricule = new System.Windows.Forms.TextBox();
+            this.txtExam1 = new System.Windows.Forms.TextBox();
+            this.txtExam2 = new System.Windows.Forms.TextBox();
+            this.txtExam3 = new System.Windows.Forms.TextBox();
+            this.txtTravail2 = new System.Windows.Forms.TextBox();
+            this.txtTravail1 = new System.Windows.Forms.TextBox();
+            this.txtTravail4 = new System.Windows.Forms.TextBox();
+            this.txtTravail3 = new System.Windows.Forms.TextBox();
+            this.txtROuE = new System.Windows.Forms.TextBox();
+            this.txtNoteEtudiant = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCalculerNote
+            // 
+            this.btnCalculerNote.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCalculerNote.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCalculerNote.Location = new System.Drawing.Point(40, 333);
+            this.btnCalculerNote.Name = "btnCalculerNote";
+            this.btnCalculerNote.Size = new System.Drawing.Size(222, 39);
+            this.btnCalculerNote.TabIndex = 0;
+            this.btnCalculerNote.Text = "Calculer la note";
+            this.btnCalculerNote.UseVisualStyleBackColor = false;
+            this.btnCalculerNote.Click += new System.EventHandler(this.btnCalculerNote_Click);
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(40, 103);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(222, 27);
+            this.txtNom.TabIndex = 1;
+            // 
+            // txtMatricule
+            // 
+            this.txtMatricule.Location = new System.Drawing.Point(309, 103);
+            this.txtMatricule.Name = "txtMatricule";
+            this.txtMatricule.Size = new System.Drawing.Size(211, 27);
+            this.txtMatricule.TabIndex = 2;
+            // 
+            // txtExam1
+            // 
+            this.txtExam1.Location = new System.Drawing.Point(40, 188);
+            this.txtExam1.Name = "txtExam1";
+            this.txtExam1.Size = new System.Drawing.Size(169, 27);
+            this.txtExam1.TabIndex = 3;
+            // 
+            // txtExam2
+            // 
+            this.txtExam2.Location = new System.Drawing.Point(309, 188);
+            this.txtExam2.Name = "txtExam2";
+            this.txtExam2.Size = new System.Drawing.Size(169, 27);
+            this.txtExam2.TabIndex = 4;
+            // 
+            // txtExam3
+            // 
+            this.txtExam3.Location = new System.Drawing.Point(550, 188);
+            this.txtExam3.Name = "txtExam3";
+            this.txtExam3.Size = new System.Drawing.Size(169, 27);
+            this.txtExam3.TabIndex = 5;
+            // 
+            // txtTravail2
+            // 
+            this.txtTravail2.Location = new System.Drawing.Point(309, 268);
+            this.txtTravail2.Name = "txtTravail2";
+            this.txtTravail2.Size = new System.Drawing.Size(169, 27);
+            this.txtTravail2.TabIndex = 7;
+            // 
+            // txtTravail1
+            // 
+            this.txtTravail1.Location = new System.Drawing.Point(40, 268);
+            this.txtTravail1.Name = "txtTravail1";
+            this.txtTravail1.Size = new System.Drawing.Size(169, 27);
+            this.txtTravail1.TabIndex = 6;
+            // 
+            // txtTravail4
+            // 
+            this.txtTravail4.Location = new System.Drawing.Point(819, 268);
+            this.txtTravail4.Name = "txtTravail4";
+            this.txtTravail4.Size = new System.Drawing.Size(169, 27);
+            this.txtTravail4.TabIndex = 9;
+            // 
+            // txtTravail3
+            // 
+            this.txtTravail3.Location = new System.Drawing.Point(550, 268);
+            this.txtTravail3.Name = "txtTravail3";
+            this.txtTravail3.Size = new System.Drawing.Size(169, 27);
+            this.txtTravail3.TabIndex = 8;
+            // 
+            // txtROuE
+            // 
+            this.txtROuE.Location = new System.Drawing.Point(309, 425);
+            this.txtROuE.Name = "txtROuE";
+            this.txtROuE.Size = new System.Drawing.Size(169, 27);
+            this.txtROuE.TabIndex = 11;
+            // 
+            // txtNoteEtudiant
+            // 
+            this.txtNoteEtudiant.Location = new System.Drawing.Point(40, 425);
+            this.txtNoteEtudiant.Name = "txtNoteEtudiant";
+            this.txtNoteEtudiant.Size = new System.Drawing.Size(169, 27);
+            this.txtNoteEtudiant.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(40, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nom de l\'étudiant(e)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(309, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Matricule";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(40, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Examen 1 X/20";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(309, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Examen 2 X/20";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(550, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 25);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Examen 3 X/20";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(40, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 25);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Travail 1 X/20";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(309, 240);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 25);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Travail 2 X/10";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(550, 240);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(129, 25);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Travail 3 X/20";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(819, 240);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(129, 25);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Travail 4 X/15";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(40, 397);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(191, 25);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Note de l\'étudiant(e)";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(309, 397);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(164, 25);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Réussite ou échec";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnExit.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnExit.Location = new System.Drawing.Point(336, 333);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(222, 39);
+            this.btnExit.TabIndex = 23;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(1069, 487);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtROuE);
+            this.Controls.Add(this.txtNoteEtudiant);
+            this.Controls.Add(this.txtTravail4);
+            this.Controls.Add(this.txtTravail3);
+            this.Controls.Add(this.txtTravail2);
+            this.Controls.Add(this.txtTravail1);
+            this.Controls.Add(this.txtExam3);
+            this.Controls.Add(this.txtExam2);
+            this.Controls.Add(this.txtExam1);
+            this.Controls.Add(this.txtMatricule);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.btnCalculerNote);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Les structures séquentielles et alternatives";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculerNote;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtMatricule;
+        private System.Windows.Forms.TextBox txtExam1;
+        private System.Windows.Forms.TextBox txtExam2;
+        private System.Windows.Forms.TextBox txtExam3;
+        private System.Windows.Forms.TextBox txtTravail2;
+        private System.Windows.Forms.TextBox txtTravail1;
+        private System.Windows.Forms.TextBox txtTravail4;
+        private System.Windows.Forms.TextBox txtTravail3;
+        private System.Windows.Forms.TextBox txtROuE;
+        private System.Windows.Forms.TextBox txtNoteEtudiant;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Les structures séquentielles et alternatives/Les structures séquentielles et alternatives/Les structures séquentielles et alternatives.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Les_structures_séquentielles_et_alternatives
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculerNote_Click(object sender, EventArgs e)
+        {
+            double Exam1, Exam2, Exam3, Trav1, Trav2, Trav3, Trav4, NoteExam, NoteTrav, NoteFin;
+            bool Resultat;
+
+            Resultat = double.TryParse(txtExam1.Text, out Exam1);
+            if (Resultat == false)
+            {
+                MessageBox.Show("Veuiller verifier si la note de l'examen #1 #1 est valide ou si la case est vide.",
+                    "Ereur de validation note!");
+                return;
+            }
+
+            Resultat = double.TryParse(txtExam2.Text, out Exam2);
+            if (Resultat == false)
+            {
+                MessageBox.Show("Veuiller verifier si la note de l'examen #2 #1 est valide ou si la case est vide.",
+                    "Ereur de validation note!");
+                return;
+            }
+
+            Resultat = double.TryParse(txtExam3.Text, out Exam3);
+            if (Resultat == false)
+            {
+                MessageBox.Show("Veuiller verifier si la note de l'examen #3 est valide ou si la case est vide.",
+                    "Ereur de validation note!");
+                return;
+            }
+
+            Resultat = double.TryParse(txtTravail1.Text, out Trav1);
+            if (Resultat == false)
+            {
+                MessageBox.Show("Veuiller verifier si la note du travail #1 est valide ou si la case est vide.",
+                    "Ereur de validation note!");
+                return;
+            }
+
+            Resultat = double.TryParse(txtTravail2.Text, out Trav2);
+            if (Resultat == false)
+            {
+                MessageBox.Show("Veuiller verifier si la note du travail #2 est valide ou si la case est vide.",
+                    "Ereur de validation note!");
+                return;
+            }
+
+            Resultat = double.TryParse(txtTravail3.Text, out Trav3);
+            if (Resultat == false)
+            {
+                MessageBox.Show("Veuiller verifier si la note du travail #3 est valide ou si la case est vide.",
+                    "Ereur de validation note!");
+                return;
+            }
+
+            Resultat = double.TryParse(txtTravail4.Text, out Trav4);
+            if (Resultat == false)
+            {
+                MessageBox.Show("Veuiller verifier si la note du travail #4 est valide ou si la case est vide.",
+                    "Ereur de validation note!");
+                return;
+            }
+
+            NoteExam = (Exam1 + Exam2 + Exam3) * 100 / 70;
+            NoteTrav = (Trav1 + Trav2 + Trav3 + Trav4) * 100 / 65;
+            NoteFin = (NoteExam * 80 / 100) + (NoteTrav * 20 / 100);
+
+            txtNoteEtudiant.Text = String.Format("{0:0.00}", NoteFin);
+
+            if (NoteFin >= 60)
+            {
+                txtROuE.Text = "Réussite";
+            }
+            else
+            {
+                txtROuE.Text = "Échec";
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
